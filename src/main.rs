5731e6b2308d103bab825c6a31cb5bb46e9af08d
Ryan Ruckley <rruckley@gmail.com>
2023-10-17 15:34:06 +1100

    Got working

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,10 @@
 use env_logger;
 use log::info;
 use actix_web::middleware::Logger;
-use actix_web::{post, HttpRequest, HttpResponse, HttpServer, App, Responder};
+use actix_web::{post, HttpRequest, HttpResponse, HttpServer, App, Responder, web};
 use std::fs;
-use mustache::Data;
 use mustache::Template;
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 
 mod common;
 mod model;
@@ -19,7 +18,7 @@ use common::error::WhiskerError;
 
 use std::io;
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 struct EmailSchema {
     title: String,
     name: String,
@@ -27,18 +26,16 @@ struct EmailSchema {
 
 #[post("/api/generate/email")]
 async fn generate_email(
+    body: web::Json<EmailSchema>,
     req: actix_web::HttpRequest,
 ) -> impl Responder {
     let html_file = fs::read_to_string("template.html").expect("Could not open template");
     
     let template = mustache::compile_str(html_file.as_str()).unwrap();
 
-    let data = EmailSchema {
-        title: String::from("A Title"),
-        name: String::from("Ryan"),
-    };
+    let api_data = body.into_inner();
     
-    let html = template.render_to_string(&data).unwrap();
+    let html = template.render_to_string(&api_data).unwrap();
 
     HttpResponse::Ok().body(html)
 }
